470999615bcfbce4c17428579934cd2cc2170a2a
Shibin Thomas <sshibinthomass@gmail.com>
2020-05-19 17:31:36 +0530

    third commit

--- enroll.py
+++ enroll.py
@@ -5,16 +5,27 @@ def create_form():
     """Creates a form
     """
     form_name = input("Enter the form name:  ")
-    questions = request_questions()
-    save_form(form_name, questions())
+    question = request_questions()
+    save_form(form_name, question)
 
 
 def request_questions():
-    questions = input("Enter your name")
-    return questions
+    i = 0
+    question = []
+    while True:
+        exit_while = input("Do you want to add another input (y/n)? ")
+        if exit_while.lower() == "n":
+            break
+        else:
+            question.append(input("Enter question {}:".format(i + 1)))
+            i += 1
+    return question
 
 
-def save_form(form_name, questions):
+def save_form(form_name, question):
     "Saves the form in json."
-    pass
+    with open(str(form_name) + ".json", "w") as f:
+        json.dump(question, f, indent=2)
 
+
+create_form()
